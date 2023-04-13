9c173ddfd6ef120ebbff16611f21b287d2395063
Felipe Espinoza <domingues.espinoza@outlook.com>
2023-04-13 11:48:32 -0300

    fix: limitação de casas decimais

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ function atualizaTabela (){
 function atualizaMediaFinal (){
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('mediaFinalValor').innerHTML = mediaFinal;
+    document.getElementById('mediaFinalValor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('mediaFinalResultado').innerHTML = mediaFinal >= notaMinima ? spamAprovado : spamReprovado;
 }
 
